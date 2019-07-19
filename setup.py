105a2860795d13d8a58ff421fcce052f3c5df250
Achilleas Athanasiou Fragkoulis <afragkoulis@hotels.com>
2019-07-19 17:24:34 +0100

    Incrementing version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(name='qubolepystream',
-      version='0.1',
+      version='0.2',
       description='Simple and easy to use Qubole connector',
       url='https://github.com/achilleasatha/PyQubole',
       author='Achilleas Athanasiou Fragkoulis',
