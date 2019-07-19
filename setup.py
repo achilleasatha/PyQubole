bfe420936def8a73f1ca81d67196bf38ab2b6ef7
Achilleas Athanasiou Fragkoulis <afragkoulis@hotels.com>
2019-07-19 17:23:41 +0100

    adding longdescription for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import setuptools
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(name='qubolepystream',
       version='0.1',
