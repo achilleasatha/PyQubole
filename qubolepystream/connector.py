2942635646152211e92626be682de6740d8b1bc2
Achilleas Athanasiou Fragkoulis <afragkoulis@hotels.com>
2020-02-27 17:08:00 +0000

    Adding new method to run Spark jobs and refactoring get_status to dedupe code, now cancelled status raises an exception

--- qubolepystream/connector.py
+++ qubolepystream/connector.py
@@ -1,6 +1,6 @@
 import os
 from qds_sdk.qubole import Qubole
-from qds_sdk.commands import HiveCommand, PrestoCommand
+from qds_sdk.commands import HiveCommand, PrestoCommand, SparkCommand
 from io import BytesIO
 import re
 import time
@@ -26,15 +26,18 @@ class QuboleConnector:
                 api_token = os.environ.get('QUBOLE_API_TOKEN')
         return api_token
 
-    def query_data(self, sql_query, job_id=None, engine=None, cluster=None, verbose=False):
+    def query_data(self, sql_query, job_id=None, engine=None, cluster=None, verbose=False, **kwargs):
         if sql_query is None or sql_query == '':
             raise RuntimeError('Query is empty')
         if job_id:
             cmd = self.get_completed_job(job_id=job_id)
         elif engine == 'Hive':
-            cmd = self.read_data_from_hive(sql_query, cluster, verbose)
+            cmd = self.read_data_from_hive(sql_query, cluster, verbose, **kwargs)
         elif engine == 'Presto':
-            cmd = self.read_data_from_presto(sql_query, cluster, verbose)
+            cmd = self.read_data_from_presto(sql_query, cluster, verbose, **kwargs)
+        elif engine == 'Spark':
+            cmd = self.run_spark_job(sql_query, cluster, verbose, **kwargs)
+            return print('Command successfully executed, command_id=%s' % cmd.attributes.get('id'))
         else:
             raise Exception('Both cluster and engine need to be specified!')
         fh = BytesIO()
@@ -45,28 +48,34 @@ class QuboleConnector:
         col_names = [re.sub(r'.+\.', '', col['ColumnName']) for col in cols]  # regex formats names for aliased tables
         return pd.read_csv(fh, delimiter=chr(9), names=col_names, na_values='\\N')
 
-    def read_data_from_hive(self, query, cluster, verbose=False):
+    def read_data_from_hive(self, query, cluster, verbose=False, **kwargs):
         print('Running Hive query')
-        cmd = HiveCommand.create(query=query, print_logs_live=True, label=cluster)
+        cmd = HiveCommand.create(query=query, print_logs_live=True, label=cluster, **kwargs)
         while cmd.attributes.get('status', None) != 'done':
             if verbose:
                 cmd = self._get_logs(cmd)
             else:
                 cmd = self._get_status(cmd)
-            if cmd.attributes['status'] == 'error':
-                raise RuntimeError('Job Failed')
         return cmd
 
-    def read_data_from_presto(self, query, cluster, verbose=False):
+    def read_data_from_presto(self, query, cluster, verbose=False, **kwargs):
         print('Running Presto query')
-        cmd = PrestoCommand.create(query=query, label=cluster)
+        cmd = PrestoCommand.create(query=query, label=cluster, **kwargs)
         while cmd.attributes.get('status', None) != 'done':
             if verbose:
                 cmd = self._get_logs(cmd)
             else:
                 cmd = self._get_status(cmd)
-            if cmd.attributes['status'] == 'error':
-                raise RuntimeError('Job Failed')
+        return cmd
+
+    def run_spark_job(self, query, cluster, verbose=False, **kwargs):
+        print('Running Spark job')
+        cmd = SparkCommand.create(query=query, label=cluster, **kwargs)
+        while cmd.attributes.get('status', None) != 'done':
+            if verbose:
+                cmd = self._get_logs(cmd)
+            else:
+                cmd = self._get_status(cmd)
         return cmd
 
     def _get_logs(self, cmd):
@@ -76,11 +85,19 @@ class QuboleConnector:
         cmd = HiveCommand.find(cmd.id)
         self.old_std_out = new_std_out
         time.sleep(5)
+        if cmd.attributes['status'] == 'error':
+            raise RuntimeError('Job Failed')
+        if cmd.attributes['status'] == 'cancelled':
+            raise RuntimeError('Job Cancelled')
         return cmd
 
     def _get_status(self, cmd):
         old_status = self.status
         self.status = cmd.attributes['status']
+        if self.status == 'error':
+            raise RuntimeError('Job Failed')
+        if self.status == 'cancelled':
+            raise RuntimeError('Job Cancelled')
         if old_status != self.status:
             print(self.status)
         cmd = cmd.find(cmd.attributes['id'])
